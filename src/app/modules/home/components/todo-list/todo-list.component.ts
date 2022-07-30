7e9760bac61460c605f21d835b9f33b7e1081a93
Gustavo98Soares <gustavo_soares98@outlook.com>
2022-07-29 23:04:12 -0300

    Projeto to-do list

---
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -0,0 +1,43 @@
+import { Component, DoCheck} from '@angular/core';
+
+//Interface
+import { TaskList } from '../../model/task-list';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements DoCheck {
+
+  public taskList: Array <TaskList> = JSON.parse(localStorage.getItem("list") || '[]');
+  constructor() { }
+
+  ngDoCheck(): void {
+    this.setLocalStorage();  
+  }
+  
+
+  public setEmitTaskList(event: string){
+    this.taskList.push({task: event, checked: false});
+  }
+
+  public deleteItemTaskList(event: number){
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList(){
+    const confirm = window.confirm("Deseja realmente deletar tudo?");
+    if(confirm){
+      this.taskList = [];
+    }
+  }
+
+  public setLocalStorage(){
+    if(this.taskList){
+      this.taskList.sort((first, last)=> Number(first.checked) -  Number(last.checked));
+      localStorage.setItem("list", JSON.stringify(this.taskList));
+    }
+  }
+
+}
